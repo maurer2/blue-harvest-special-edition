980a135bdb0128d586ae2d01ce953689f6bcdabb
Martin <mr.mmaurer@gmail.com>
2024-11-24 15:15:23 +0100

    Added fetch helper

---
+++ src/app/helpers/fetcher/index.ts
@@ -0,0 +1,29 @@
+import z, { ZodError } from 'zod';
+
+// https://github.com/colinhacks/zod#writing-generic-functions
+export default async function fetcher<T extends z.ZodTypeAny>(url: string, schema?: T) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Error fetching data');
+    }
+
+    const data = await response.json();
+
+    if (!schema) {
+      return data;
+    }
+
+    const parsedData = schema.parse(data);
+    return parsedData;
+  } catch (error) {
+    if (error instanceof Error || error instanceof ZodError) {
+      console.warn(error.message);
+      throw new Error(error.message, { cause: error });
+    }
+
+    console.warn('Unknown error');
+    throw new Error('Unknown error');
+  }
+}
