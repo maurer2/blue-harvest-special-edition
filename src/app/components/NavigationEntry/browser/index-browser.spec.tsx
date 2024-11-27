caf4e4dea604efdc298d7036c6671f459f851f7d
Martin <mr.mmaurer@gmail.com>
2024-11-28 00:31:13 +0100

    Checked out vitest's new browser mode

---
+++ src/app/components/NavigationEntry/browser/index-browser.spec.tsx
@@ -0,0 +1,25 @@
+import { expect, describe, it, vi } from 'vitest';
+import type { ComponentPropsWithoutRef } from 'react';
+import { render } from 'vitest-browser-react';
+
+import NavigationEntry from '../index';
+
+type NavigationEntryProps = ComponentPropsWithoutRef<typeof NavigationEntry>;
+
+vi.mock('next/navigation', () => ({
+  usePathname() {
+    return '/categories/current-page/1';
+  },
+}));
+
+describe('Navigation Entry', () => {
+  const props: NavigationEntryProps = {
+    name: 'current-page',
+  };
+
+  it('renders name', async () => {
+    const { getByText } = render(<NavigationEntry {...props} />);
+
+    await expect.element(getByText('current-page')).toBeInTheDocument();
+  });
+});
--- vitest.config.mts
+++ vitest.config.mts
@@ -4,8 +4,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  define: {
+    'process.env': JSON.stringify({}), // https://github.com/vitest-dev/vitest/issues/6872#issuecomment-2461113494
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test-setup.ts',
+    browser: {
+      enabled: false,
+      name: 'chromium',
+      provider: 'playwright',
+    },
   },
 });
